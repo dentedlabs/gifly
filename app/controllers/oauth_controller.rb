ce479d7c50eba2c406d239fd8a3232bb125033d2
Gram <g@wusi.com>
2015-09-03 19:20:58 +0800

    added new webhook fields to team

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -8,13 +8,15 @@ class OauthController < ApplicationController
       redirect_uri: "http://gifly.dented.io/oauth/slack/callback" #request.original_url.split('?')[0]
     }
     response = JSON.parse(RestClient.post(CONFIG.slack.access_token_url, post_params))
-    Rails.logger.error("Slack Response #{response.inspect} #{post_params.inspect}")
+    Rails.logger.error("Slack Response #{response.inspect}")
     if(response['ok'])
       update_team_attributes = {
+        name: response['team_name'],
         scope: response['scope'],
         code: slack_params[:code],
-        channel_id: response['channel_id'],
-        webhook_url: response['incoming_webhook']
+        channel_id: response['incoming_webhook']['channel'],
+        configuration_url: response['incoming_webhook']['configuration_url'],
+        webhook_url: response['incoming_webhook']['url']
       }
       team = Team.where(access_token: response['access_token'].to_s).first_or_create!(update_team_attributes)
       render_success_response(serialize_team(team))
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,8 +4,10 @@ class Team
 
   # field :id, type: String, uniq: true
   field :code, type: String
+  field :name, type: String
   field :access_token, type: String, uniq: true, index: true
   field :scope, type: String
   field :channel_id, type: String
+  field :configuration_url, type: String
   field :webhook_url, type: String
 end
